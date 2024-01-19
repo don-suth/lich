c36e33ca15ff5382b837cac6d980645af88b591b
donsuth <donald@sutherland.id.au>
2024-01-19 11:14:47 +0800

    Test uploading image via embeds

--- commands/new_webcams.py
+++ commands/new_webcams.py
@@ -1,6 +1,7 @@
 import discord
 from discord import ui, Interaction
-import asyncio
+from aiohttp import ClientSession
+from io import BytesIO
 
 
 DONALD_ID = 243405584651517954
@@ -10,13 +11,27 @@ def check_if_its_me(interaction: discord.Interaction):
 	return interaction.user.id == DONALD_ID
 
 
+async def get_image(image_url, session, filename):
+	image_bytes = BytesIO()
+	async with session.get(image_url) as response:
+		image_bytes.write(await response.read())
+	discord_file = discord.File(image_bytes, filename=filename)
+	return discord_file
+
+
 class InputBox(ui.Modal, title="Enter Webcam Password:"):
 	password = ui.TextInput(label="Webcam Password", style=discord.TextStyle.short)
 
 	async def on_submit(self, interaction: Interaction) -> None:
-		await interaction.response.defer(ephemeral=False, thinking=True)
-		await asyncio.sleep(5)
-		await interaction.followup.send(f"Success! The password was {self.password}")
+		if self.password != "supersecret":
+			await interaction.response.send_message("Sorry, the password you entered was incorrect.", ephemeral=True)
+		else:
+			await interaction.response.defer(ephemeral=False, thinking=True)
+			async with ClientSession() as session:
+				test_image = await get_image(image_url="https://preview.redd.it/5l9c34dl57q21.png", session=session, filename="voja.png")
+			embed = discord.Embed(title="Voja")
+			embed.set_image(url="attachment://voja.png")
+			await interaction.followup.send(embed=embed, files=[test_image])
 
 
 @discord.app_commands.command(description="Testing the new webcam commands.")

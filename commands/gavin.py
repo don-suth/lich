ed4d21d4628f1a1ba290c3a5ab1ad741d4313193
donsuth <donald@sutherland.id.au>
2024-10-29 13:13:44 +0800

    Update Gavin to Cog

--- commands/gavin.py
+++ commands/gavin.py
@@ -1,28 +1,52 @@
 import discord
+from discord import app_commands
+from discord.ext import commands
 
 
-class ClubroomGroup(discord.app_commands.Group):
-	@discord.app_commands.command(description="Let us guide you to Unigames from the Tav!")
-	async def tav(self, interaction: discord.Interaction):
-		await interaction.response.send_message("Follow us from the Tav!\nhttps://www.youtube.com/watch?v=ufswr_-B3tA")
-
-	@discord.app_commands.command(description="Let us guide you to Unigames from the Guild Village!")
-	async def guild_village(self, interaction: discord.Interaction):
-		await interaction.response.send_message("Follow us from the Guild Village!\nhttps://www.youtube.com/watch?v=O0SrGr8TwBU")
-
-
-class OldGavinGroup(discord.app_commands.Group):
-	@discord.app_commands.command(description="(Outdated) Let Gavin guide you to Unigames from the Tav!")
+class ClubroomCog(commands.GroupCog, group_name="clubroom"):
+	@app_commands.command(description="Let us guide you to Unigames from the Tav!")
 	async def tav(self, interaction: discord.Interaction):
 		await interaction.response.send_message(
-			"Follow Gavin from the Tav!\nhttps://www.youtube.com/watch?v=sg4XJG8HGTw")
+			"Follow us from the Tav!\nhttps://www.youtube.com/watch?v=ufswr_-B3tA"
+		)
 
-	@discord.app_commands.command(description="(Outdated) Let Gavin guide you to Unigames from the middle of Guild Village!")
+	@app_commands.command(description="Let us guide you to Unigames from the Guild Village!")
 	async def guild_village(self, interaction: discord.Interaction):
 		await interaction.response.send_message(
-			"Follow Gavin from Guild Village!\nhttps://www.youtube.com/watch?v=Jaw6zF5cB7Q")
+			"Follow us from the Guild Village!\nhttps://www.youtube.com/watch?v=O0SrGr8TwBU"
+		)
+
+	async def cog_load(self):
+		print(f"\t - {self.__class__.__name__} loaded")
+
+	async def cog_unload(self):
+		print(f"\t - {self.__class__.__name__} unloaded")
 
 
-async def setup(bot):
-	bot.tree.add_command(OldGavinGroup(name="old_gavin", description="(Outdated) Let Gavin guide you to Unigames..."))
-	bot.tree.add_command(ClubroomGroup(name="clubroom", description="Let us guide you to the Unigames clubroom..."))
+class OldGavinCog(commands.GroupCog, group_name="old_gavin"):
+	@app_commands.command(
+		description="(Outdated) Let Gavin guide you to Unigames from the Tav!"
+	)
+	async def tav(self, interaction: discord.Interaction):
+		await interaction.response.send_message(
+			"Follow Gavin from the Tav!\nhttps://www.youtube.com/watch?v=sg4XJG8HGTw"
+		)
+
+	@app_commands.command(
+		description="(Outdated) Let Gavin guide you to Unigames from the middle of Guild Village!"
+	)
+	async def guild_village(self, interaction: discord.Interaction):
+		await interaction.response.send_message(
+			"Follow Gavin from Guild Village!\nhttps://www.youtube.com/watch?v=Jaw6zF5cB7Q"
+		)
+
+	async def cog_load(self):
+		print(f"\t - {self.__class__.__name__} loaded")
+
+	async def cog_unload(self):
+		print(f"\t - {self.__class__.__name__} unloaded")
+
+
+async def setup(bot: commands.Bot):
+	await bot.add_cog(ClubroomCog(bot=bot))
+	await bot.add_cog(OldGavinCog(bot=bot))

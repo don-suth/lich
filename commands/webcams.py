42afd733390fa080a35995ee687d568f9ae63d0f
donsuth <donald@sutherland.id.au>
2024-10-29 12:06:22 +0800

    Change webcams to cog

--- commands/webcams.py
+++ commands/webcams.py
@@ -1,9 +1,25 @@
 import discord
+from discord import app_commands
+from discord.ext import commands
 
-@discord.app_commands.command(description="Deprecated")
-@discord.app_commands.guild_only()
-async def webcams(interaction: discord.Interaction):
-	await interaction.response.send_message("The Webcams are no longer viewable due to UWA Student Guild regulations. If you have further questions, please contact Committee.", ephemeral=True)
 
-async def setup(bot):
-	bot.tree.add_command(webcams)
+@app_commands.guild_only()
+class WebcamsCog(commands.Cog):
+
+	@app_commands.command(description="Deprecated")
+	async def webcams(self, interaction: discord.Interaction):
+		await interaction.response.send_message(
+			"The Webcams are no longer viewable due to UWA Student Guild regulations. "
+			"If you have further questions, please contact Committee.",
+			ephemeral=True
+		)
+
+	async def cog_load(self):
+		print(f"\t - {self.__class__.__name__} loaded")
+
+	async def cog_unload(self):
+		print(f"\t - {self.__class__.__name__} unloaded")
+
+
+async def setup(bot: commands.Bot):
+	await bot.add_cog(WebcamsCog(bot=bot))
